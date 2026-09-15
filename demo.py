d6328cb3550edc776691394c30e1e6d7b28d588f
Swapkudale <Swapnali.26.sk@gmail.com>
2026-09-15 18:03:31 +0530

    demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,6 @@
-"""Simple Python program demonstrating basic variables and conditionals."""
+
+
+"Simple Python program demonstrating basic variables and conditionals."
 
 NAME = "Swapnali"
 AGE = 30
@@ -7,6 +9,6 @@ print("Hello", NAME)
 print("Your age is", AGE)
 
 if AGE >= 18:
-print("You are an adult")
+    print("You are an adult")
 else:
-print("You are a minor")
+    print("You are a minor")
